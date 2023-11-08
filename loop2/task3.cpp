5daa73039eccd17a0fac3856a2a864f46db9fab7
cezarDi <cezaryulin@gmail.com>
2023-11-08 20:58:12 +0300

    solution to task3 from loop2.md

--- loop2/task3.cpp
+++ loop2/task3.cpp
@@ -1,3 +1,16 @@
-//
-// Created by cezar on 08.11.23.
-//
+#include <iostream>
+#include <cmath>
+
+int main() {
+    int n;
+    double x;
+    std::cin >> n >> x;
+    double s = 0;
+    double sin_value = sin(x);
+    for (int i = 1; i <= n; i++) {
+        s += sin_value;
+        sin_value = sin(sin_value);
+    }
+    std::cout << s << "\n";
+    return 0;
+}
